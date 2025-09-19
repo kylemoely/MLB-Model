fc4bb8d234871e5fd6a2a0f895dbae4df6eee77c
kylemoely <kylemoely@gmail.com>
2025-09-19 12:07:40 -0600

    Added fieldable_plays stuff

--- etl/transform/transform_game.py
+++ etl/transform/transform_game.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from .builders import builder_pitcher_innings, builder_plate_appearances
+from .builders import builder_pitcher_innings, builder_plate_appearances, builder_fieldable_plays
 from .parsers import parse_plays
 import argparse
 from dotenv import load_dotenv
@@ -26,22 +26,21 @@ def transform_game(filepath: Path):
     #filepath should be 'gameData_{gamePk}.json'
     gamePk = int(filename.replace(".json","").replace("gameData_",""))
 
-    pa_df = builder_plate_appearances(parsed_plays)
-    pi_df = builder_pitcher_innings(parsed_plays)
-    pa_df["gamepk"] = gamePk
-    pi_df["gamepk"] = gamePk
-    pa_df["game_date"] = date
-    pi_df["game_date"] = date
+    pa_df = builder_plate_appearances(parsed_plays, gamePk, date)
+    pi_df = builder_pitcher_innings(parsed_plays, gamePk, date)
+    fp_df = builder_fieldable_plays(parsed_plays, gamePk, date)
 
     pi_filepath = CLEAN_DATA_DIR / filename.replace(".json","_pitcher_innings.parquet")
     pa_filepath = CLEAN_DATA_DIR / filename.replace(".json","_plate_appearances.parquet")
+    fp_filepath = CLEAN_DATA_DIR / filename.replace(".json","_fieldable_plays.parquet")
 
-    pa_df.to_parquet(pa_filepath, engine="pyarrow", index=False)
-    pi_df.to_parquet(pi_filepath, engine="pyarrow", index=False)
+    pa_df.to_parquet(pa_filepath, index=False)
+    pi_df.to_parquet(pi_filepath, index=False)
+    fp_df.to_parquet(fp_filepath, index=False)
 
     #print(f"Successfully saved both parquet files at {pa_filepath} and {pi_filepath}.")
 
-    return pi_filepath, pa_filepath
+    return pi_filepath, pa_filepath, fp_filepath
 
 def main():
     parser = argparse.ArgumentParser()
