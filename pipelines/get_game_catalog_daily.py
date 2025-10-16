cb8fa2ffad4eb9f579b1cce70357784ffa964d41
kylemoely <kylemoely@gmail.com>
2025-10-16 08:56:35 -0600

    Created daily pipeline for getting upcoming games and loading them into game_catalog

---
+++ pipelines/get_game_catalog_daily.py
@@ -0,0 +1,18 @@
+from db.db import engine
+from etl.extract.fetch_daily_schedule import fetch_daily_schedule
+from etl.transform.transform_daily_schedule import transform_daily_schedule
+from etl.load.load_daily_schedule import load_daily_schedule
+from datetime import datetime
+
+def get_game_catalog_daily():
+    try:
+        raw_filepath = fetch_daily_schedule(datetime.now().strftime("%Y-%m-%d"))
+
+        sched_filepath = transform_daily_schedule(raw_filepath)
+
+        load_daily_schedule(sched_filepath, engine)
+    except Exception as e:
+        print(f"Daily schedule ingestion failed: {e}")
+
+if __name__ == "__main__":
+    get_game_catalog_daily()
