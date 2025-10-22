d26328575ef2576b8ec99fe9f763a9de17c72bb0
kylemoely <kylemoely@gmail.com>
2025-10-22 09:28:47 -0600

    Refactored db.py for AWS

--- db/db.py
+++ db/db.py
@@ -4,12 +4,16 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
-DB_HOST = os.getenv("DB_HOST")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+### Local Dev
+# DB_USER = os.getenv("DB_USER")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+### AWS Dev
+DB_URL = os.getenv("DB_URL")
 
 engine = create_engine(DB_URL)
