ed6497cb968d48e79e3dc8892ca15b7b833da7d6
kylemoely <kylemoely@gmail.com>
2025-10-22 10:43:27 -0600

    Refactored fetch_game for AWS

--- etl/extract/fetch_game.py
+++ etl/extract/fetch_game.py
@@ -4,17 +4,23 @@ from pathlib import Path
 import argparse
 from dotenv import load_dotenv
 import os
+import s3fs
 
 load_dotenv()
+fs = s3fs.S3FileSystem()
 
 DATA_DIR = Path(os.getenv("DATA_DIR"))
-RAW_DATA_DIR = DATA_DIR / "raw"
+RAW_DATA_DIR = DATA_DIR / "raw/game-datas"
 
 def fetch_game(gamePk: int) -> Path:
 
     game = statsapi.get("game", params={"gamePk":gamePk})
-    filepath = RAW_DATA_DIR / f"gameData_{gamePk}.json"
-    with open(filepath, "w", encoding="utf-8") as f:
+    filepath = str(RAW_DATA_DIR / f"gameData_{gamePk}.json")
+    ### Local Dev
+    # with open(filepath, "w", encoding="utf-8") as f:
+    #     json.dump(game, f, indent=4)
+    ### AWS Dev
+    with fs.open(filepath, "w", encoding="utf-8") as f:
         json.dump(game, f, indent=4)
 
     #print(f"Succesfully saved raw JSON file at {filepath}.")
