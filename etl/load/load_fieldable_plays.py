a153fffb87cdb2eccaf0814b43f92d4d3f8d2d0c
kylemoely <kylemoely@gmail.com>
2025-10-24 16:10:46 -0600

    Changed num_outs column type to string to satisfy postgres enum type

--- etl/load/load_fieldable_plays.py
+++ etl/load/load_fieldable_plays.py
@@ -7,4 +7,6 @@ def load_fieldable_plays(filepath, engine):
     if len(df)==0 or list(df.columns)!=["launch_speed","launch_angle","total_distance","trajectory","hardness","hit_location","coord_x","coord_y","fielder","fielder_id","putouter","putouter_id", "errer","errer_id","in_play_out","pickoff_out","has_out","has_score","first_base_runner","second_base_runner","third_base_runner","num_outs","responsibility","gamepk"]:
         raise Exception(f"Improper dataframe from {filepath}")
 
+    df["num_outs"] = df["num_outs"].astype("str")
+
     df.to_sql("fieldable_plays",engine, if_exists="append", index=False)
